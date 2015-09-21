38f305017483a5bb7924dc3738fd047d44456385
lilutanya <lilu.tanya@gmail.com>
2015-09-21 15:58:18 +0300

    Fix test

--- test.js
+++ test.js
@@ -1,5 +1,5 @@
- var test = require('tap').test;
- var magic = require('./mathlib');
+var test = require('tap').test;
+var magic = require('./mathlib');
 
 test("Loading", function (t) {
   math = require("./mathlib");
@@ -8,19 +8,19 @@ test("Loading", function (t) {
 });
 
 test("Validate getNumber result", function (t) {
-  t.plan(3);		
-	magic.getNumber(5, 7, function (err, data) {
+  t.plan(3);
+  magic.getNumber(5, 7, function (err, data) {
     t.notOk(err, 'No errors');
     t.equal(Number.isNaN(data), false, 'Valid number');
-	  t.ok(data, 'Data ok');
-	});
+    t.ok(data, 'Data ok');
+  });
 });	
 
 test("Validate getSqrt result", function (t) {
-  t.plan(3);		
-	magic.getSqrt(9, function (err, data) {
+  t.plan(3);
+  magic.getSqrt(9, function (err, data) {
     t.notOk(err, 'No errors');
-	  t.ok(data, 'Data ok');
-	  t.equal(data, 3, "Valid result");
-	});
+    t.ok(data, 'Data ok');
+    t.equal(data, 3, "Valid result");
+  });
 });	
